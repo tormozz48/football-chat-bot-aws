30cad3b5c7a695a6f1f40cd79a0b6685d3686898
tormozz48 <tormozz48@gmail.com>
2022-05-02 00:16:09 +0400

    setup unit tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,29 +1,4 @@
-import * as AWS from 'aws-sdk';
-import { offlineOptions } from '../src/database/dynamodb';
-
-const dynamodb = new AWS.DynamoDB(offlineOptions);
-
-const waitForDynamoDbToStart = async () => {
-  try {
-    await dynamodb.listTables().promise();
-  } catch (error) {
-    console.log('Waiting for Docker container to start...');
-    await new Promise((resolve) => setTimeout(resolve, 500));
-    return waitForDynamoDbToStart();
-  }
-};
-
 module.exports = async () => {
   process.env.IS_OFFLINE = 'true';
   process.env.EVENTS_TABLE = 'events-table-dev';
-
-  const start = Date.now();
-  try {
-    await waitForDynamoDbToStart();
-    console.info(`DynamoDB-local started after ${Date.now() - start}ms.`);
-    process.exit(0);
-  } catch (error) {
-    console.info('Error starting DynamoDB-local!', error);
-    process.exit(1);
-  }
 };
