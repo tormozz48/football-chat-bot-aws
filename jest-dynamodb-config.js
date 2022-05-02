54d4e833756c01df239324974542204a2ccdedfd
tormozz48 <tormozz48@gmail.com>
2022-05-02 21:32:37 +0400

    Fix up

---
+++ jest-dynamodb-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  tables: [
+    {
+      TableName: `events-table-dev`,
+      KeySchema: [
+        { AttributeName: 'chatId', KeyType: 'HASH' },
+        { AttributeName: 'eventDate', KeyType: 'RANGE' },
+      ],
+      AttributeDefinitions: [
+        { AttributeName: 'chatId', AttributeType: 'N' },
+        { AttributeName: 'eventDate', AttributeType: 'N' },
+      ],
+      ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
+    },
+  ],
+  installerConfig: {
+    installPath: './docker/dynamodb',
+  },
+  port: 8000,
+};
